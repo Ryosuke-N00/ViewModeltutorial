bc25b897b925f64d8be0ce744c709560dff84950
Air-forse1 <Timber123987456@gmail.com>
2020-12-27 13:56:33 +0900

    I outputed how to use Livedata also, this file is not useful for your study so, it's waste time if you watch this.

--- app/src/main/java/com/example/ryousuke/myapplication/faragment/GameFragment.kt
+++ app/src/main/java/com/example/ryousuke/myapplication/faragment/GameFragment.kt
@@ -16,6 +16,9 @@ class GameFragment:Fragment() {
     //フレームワークから情報を与えられるまで変数の初期化を遅らせる
     private lateinit var viewModel:GameViewModel
 
+    //DataBindingを利用するときに初期化が必要 GameFragmentBindingは、レイアウトファイルにDataBindingを関連付けるとき作成される
+    //private lateinit var binding: GameFragmentBinding
+
     //ViewModelからデータに関する情報をもらうのでbindingを定義する必要がある
     //private lateinit var binding:GameFragmentBinding→レイアウトに必要な処理を実行しておくと勝手にこのクラスを作ってくれる
 
@@ -25,16 +28,33 @@ class GameFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        //Inflateして、bindingクラスのオブジェクトを手に入れる
+        //binding= DataBindingUtil.inflate(
+        //                inflater,
+        //                R.layout.game_fragment,
+        //                container,
+        //                false
+        //        )
+
         //Fragment生成されたときにきちんと作成されているかどうかの確認
-        Log.i("GameFragment","GameFragment is created!!")
+        Log.i("GameFragment", "GameFragment is created!!")
 
         //初期化したViewModelをFragment作成時に呼び出す
         //ここでのLog表示は必要ない、initブロックでLog表示されるようになっているからだ
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
+        //viewmodel のためにbindingを設定することで、レイアウトファイルが直接viewmodelにアクセスできる
+        //binding.gameViewModel = viewModel
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        //フラグメントは、bindingのライフサイクルオーナーで、LiveDataの変化をここで気づく
+        //binding.lifecycleOwner = viewLifecycleOwner
+
+
+        //return binding.root
+
+
     }
+
 
     //以下必要なメソッドがあるが省略
 
--- app/src/main/java/com/example/ryousuke/myapplication/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/example/ryousuke/myapplication/viewmodel/GameViewModel.kt
@@ -1,21 +1,43 @@
 package com.example.ryousuke.myapplication.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 //ViewModelを作成するときは、ViewModelを継承させる
 class GameViewModel: ViewModel() {
 
     //類推で型の省略ができ、データとしてどんな情報を持たせておくかを決める
-    var word = ""
-    var score = 0
-    //以下に必要なメソッドの定義をしていくが省略
+    //var word = "" ,var score = 0をViewModelが持つのではなくLiveDataが管理する
 
+    //読み取り専用のMutableList,外部ファイルからデータの読み取り専用のLiveData
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    //wordList中に読み取り専用のリストを作成
+    private lateinit var wordList: MutableList<String>
+
+    private fun resetList(){
+        wordList = mutableListOf(
+                "Sato",
+                "Suzuki",
+                "Nakamura"
+        )
+        //中身を並べ替える
+        wordList.shuffle()
+    }
 
     //ViewModel作成時にLogに表示してくれる内容
+    //初期化するとき、LiveDataの持つデータはvalueとして扱う
     init {
-        Log.i("GameViewModel","GameViewModel is created!!")
+        _word.value = ""
+        Log.i("GameViewModel","GameViewModel is created")
+        resetList()
     }
+
+
 
     //フラグメントの破棄時にLog表示してくれる
     override fun onCleared() {
